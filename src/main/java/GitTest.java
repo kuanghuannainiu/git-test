f46465b7f24a02c8d80fea5620c0b02a54bbc474
wzy <1901706467@qq.com>
2021-08-08 18:20:10 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author wzy
+ * @Date 2021/8/8 18:16
+ * @Version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
